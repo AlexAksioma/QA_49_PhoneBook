d161e5c7d945623262b6d64b6131eb36fe47b08c
AlexAksioma <alexmedqwerty@gmail.com>
2025-10-30 19:22:33 +0200

    create add new contact posititve test

--- src/test/java/ui_tests/AddNewContactTests.java
+++ src/test/java/ui_tests/AddNewContactTests.java
@@ -1,9 +1,12 @@
 package ui_tests;
 
+import dto.Contact;
 import manager.ApplicationManager;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.*;
+import utils.ContactFactory;
 import utils.HeaderMenuItem;
 
 import static pages.BasePage.*;
@@ -14,6 +17,7 @@ public class AddNewContactTests extends ApplicationManager {
     LoginPage loginPage;
     ContactsPage contactsPage;
     AddPage addPage;
+    int numberOfContacts;
 
     @BeforeMethod
     public void login(){
@@ -21,11 +25,21 @@ public class AddNewContactTests extends ApplicationManager {
         loginPage = clickButtonHeader(HeaderMenuItem.LOGIN);
         loginPage.typeLoginForm("a@mail.ru", "Password123!");
         contactsPage = new ContactsPage(getDriver());
+        numberOfContacts = contactsPage.getNumberOfContacts();
         addPage = clickButtonHeader(HeaderMenuItem.ADD);
     }
 
     @Test
     public void addNewContactPositiveTest(){
+        addPage.typeContactForm(ContactFactory.positiveContact());
+        int numberOfContactsAfterAdd = contactsPage.getNumberOfContacts();
+        Assert.assertEquals(numberOfContactsAfterAdd, numberOfContacts + 1);
+    }
 
+    @Test
+    public void addNewContactPositiveTestValidateList(){
+        Contact contact = ContactFactory.positiveContact();
+        addPage.typeContactForm(contact);
+        Assert.assertTrue(contactsPage.isContactPresent(contact));
     }
 }
